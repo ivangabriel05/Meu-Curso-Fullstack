fea4c87bd313b5cb4d2b1219ad7a94f933262ce0
ivangabriel05 <minhacontaprivada07@gmail.com>
2024-03-01 07:40:54 -0300

    add de arquivos

--- Módulo 02/07 - Interação com API/Projeto API/ProjetoApi.js
+++ Módulo 02/07 - Interação com API/Projeto API/ProjetoApi.js
@@ -1,21 +1,21 @@
 async function chamar(){
-        let busca = await fetch("ProjetoApi.json")
-        let produtos = await busca.json()
-        let grupoDiv = document.getElementById("Lista-card")
-        for(let produto of produtos ){
-    
-            grupoDiv.innerHTML += `
-                <div class="card">
-                    <img src="${produto.imagem}" width="250px" height="250px">
-                    <h3>${produto.marca}</h3>
-                    <p>${produto.valor}R$</p>
-                    <p>${produto.modelo}</p>
-                    <p>${produto.sistemaOperacional}</p>
-            
-                </div>
-            `
+    let busca = await fetch("ProjetoApi.json")
+    let produtos = await busca.json()
+    let grupoDiv = document.getElementById("Lista-card")
+            for(let produto of produtos ){
+        
+                grupoDiv.innerHTML += `
+                    <div class="card">
+                        <img src="${produto.imagem}" width="250px" height="250px">
+                        <h3>${produto.marca}</h3>
+                        <p>${produto.valor}R$</p>
+                        <p>${produto.modelo}</p>
+                        <p>${produto.sistemaOperacional}</p>
+                
+                    </div>
+                `
 
-            
-        }
-    }
+                
+            }
+}
 chamar()
