d6b15b6b39679068b484b7aac232a87161125d81
mauricio santo <minhacontaprivada07@gmail.com>
2024-03-12 17:24:22 -0300

    atvd

--- Módulo 02/07 - Interação com API/Aula 07/Exemplo 02/index.js
+++ Módulo 02/07 - Interação com API/Aula 07/Exemplo 02/index.js
@@ -4,16 +4,19 @@ async function buscar(){
     let procura = await fetch("lista-produtos.json")
     let achados = await procura.json()
 
-    let grupoDiv = document.getElementById("lista-dell")
+    let grupoDiv = document.getElementById("lista-card")
         for( let produto of achados){
             grupoDiv.innerHTML += `
                 <div class="card">
-                    <h3>${produto.nome}</h3>
-                    <p>${produto.descrição}</p>
-                    <p>${produto.valorComdescoto}</p>
-                    <p>${produto.valorSemdescoto}</p>
+                    <h2>${produto.nome}</h2>
+                    <h3>${produto.descrição}</h3>
+                <div class="valores">
+                    <span>${produto.valorComdescoto.toFixed(2)}</span>
+                    <span>${produto.valorSemdescoto.toFixed(2)}</span>
                 </div>
-            `
+            </div>
+
+        `
 
     }
 }
