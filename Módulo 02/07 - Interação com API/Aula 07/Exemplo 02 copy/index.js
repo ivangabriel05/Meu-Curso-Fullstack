dce31a407df67d79ffb397731f64a3a9b82c7f03
ivangabriel <minhacontaprivada07@gmail.com>
2024-03-20 14:02:13 -0300

    atividade

--- Módulo 02/07 - Interação com API/Aula 07/Exemplo 02 copy/index.js
+++ Módulo 02/07 - Interação com API/Aula 07/Exemplo 02 copy/index.js
@@ -7,18 +7,16 @@ async function buscar(){
     let grupoDiv = document.getElementById("lista-card")
         for( let produto of achados){
             grupoDiv.innerHTML += `
-                <div class="card" data-id="${produto.id}">
+                <div class="card" data-id${produto.id}"">
                     <h2>${produto.nome}</h2>
-                    <imagem src="${produto.imagem}" width"200" heigth="auto">
                     <h3>${produto.descrição}</h3>
-                <div class="valores">
-                    <span>${produto.valorComdescoto.toFixed(2)}</span>
-                    <span>${produto.valorSemdescoto.toFixed(2)}</span>
-                </div>
+                    <div class="valores">
+                        <span>${produto.valorComdescoto.toFixed(2)}</span>
+                        <span>${produto.valorSemdescoto.toFixed(2)}</span>
+                    </div>
             </div>
-
         `
-
     }
+    let divsCards = document.getElementsByClassName("card")
 }
 buscar()
