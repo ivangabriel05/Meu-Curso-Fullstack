b2cd71a7c4645b7a6d4771c04089d691d4a4c245
gabriel <minhacontaprivada07@gmail.com>
2024-04-02 17:16:10 -0300

    atvd

--- Módulo 02/07 - Interação com API/Aula 07/Exemplo 02 copy 2/detalhes.js
+++ Módulo 02/07 - Interação com API/Aula 07/Exemplo 02 copy 2/detalhes.js
@@ -38,5 +38,4 @@ async function mucuraPreta(){
     `
     
 }
-
 mucuraPreta()
--- Módulo 02/07 - Interação com API/Aula 07/Exemplo 02 copy 2/index.js
+++ Módulo 02/07 - Interação com API/Aula 07/Exemplo 02 copy 2/index.js
@@ -33,3 +33,21 @@ function clicou(){
 mocura()
 // window
 // coleta qual que valor do atributo "data-id" do elemento HTML que acionou o evento de escurta
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("dropdown").querySelector(".dropdown-content");
+    dropdownContent.style.display === "none" ? dropdownContent.style.display = "block" : dropdownContent.style.display = "none";
+  }
+  
+  // Fechar o menu quando clicar fora dele
+  // Fechar o menu quando clicar fora dele
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-button')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.style.display === "block") {
+          openDropdown.style.display = "none";
+        }
+      }
+    }
+  }
